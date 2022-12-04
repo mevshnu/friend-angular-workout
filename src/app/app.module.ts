bf93afb30c1881114dbaef029efa9e9eb910e4ac
mevshnu <vishnusivan1000@gmail.com>
2022-12-04 20:51:09 +0530

    1st

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { ViewComponent } from './view/view.component';
+import { NavComponent } from './nav/nav.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ViewComponent,
+    NavComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
